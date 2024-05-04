d9620ef8d19f288b425f7a71bf08f47047e5f664
Irana Miriyeva <irana.miriyeva@gmail.com>
2024-05-04 02:14:17 +0000

    Understand BCrypt Hashes

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-'use strict';
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-const app         = express();
-fccTesting(app);
-const saltRounds = 12;
-const myPlaintextPassword = 'sUperpassw0rd!';
-const someOtherPlaintextPassword = 'pass123';
-
+'use strict'
+const express     = require('express')
+const bodyParser  = require('body-parser')
+const fccTesting  = require('./freeCodeCamp/fcctesting.js')
+const app         = express()
+fccTesting(app)
+const saltRounds = 12
+const myPlaintextPassword = 'sUperpassw0rd!'
+const someOtherPlaintextPassword = 'pass123'
+const bcrypt = require('bcrypt')
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
@@ -22,34 +22,7 @@ const someOtherPlaintextPassword = 'pass123';
 //END_SYNC
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log("Listening on port:", PORT)
-});
+})
